fe61f7017dd31b7cb2221ba81f26856f8a2eca55
Edwin Jose Palathinkal <edwinhere@gmail.com>
2020-06-08 18:56:39 +0800

    Fixed My.cmd

--- My.cmd
+++ My.cmd
@@ -1,1 +1,2 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File Win10.ps1 -include Win10.psm1 -preset My.preset
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File ./Win10-Initial-Setup-Script/Win10.ps1 -include ./Win10-Initial-Setup-Script/Win10.psm1 -preset ./My.preset
+
